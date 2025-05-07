f1acfe8ee54e34c9a18bba8a712870764135f3b3
Eng.Mokhtar Alahdal <58419254+alahdalmokhtar@users.noreply.github.com>
2025-05-08 01:51:04 +0200

    mof

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from rectangel import  Rectangel
 
 def main():
     rect=Rectangel(10,11)
+    rect1=Rectangel(12,3)
     print("permiter is :",Rectangel.permiter(rect))
     print("area is :",Rectangel.area(rect))
-
+    print("area is ",Rectangel.area(rect1))
 
 
 if __name__==main():
